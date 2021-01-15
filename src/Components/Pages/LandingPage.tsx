5d7cb5f5a6ff6859d23ee428f08d6139d5f5d8a7
Michał Prawda <m.prawda99@gmail.com>
2021-01-15 17:59:23 +0100

    add login form

--- src/Components/Pages/LandingPage.tsx
+++ src/Components/Pages/LandingPage.tsx
@@ -2,11 +2,16 @@ import React from 'react';
 import theme from "../../theme";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import { ThemeProvider } from "@material-ui/core/styles";
+import { Grid } from '@material-ui/core';
+import LoginForm from '../User/LoginForm';
 
 const ErrorPage = () => {
     return (
         <ThemeProvider theme={theme}>
         <CssBaseline />
+        <Grid container justify = "center">
+            <LoginForm />
+        </Grid>
     </ThemeProvider>
     )
 }
---
+++ src/Components/User/LoginForm.tsx
@@ -0,0 +1,51 @@
+import { makeStyles, Theme, createStyles, TextField, Button, Grid } from '@material-ui/core';
+import React from 'react'
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      '& .MuiTextField-root': {
+        margin: theme.spacing(1),
+        width: 300,
+      },
+      marginTop: 200
+    },
+    button: {
+      margin: 3
+    }
+  }),
+);
+
+const LoginForm = () => {
+  const classes = useStyles();
+
+  return (
+    <form className={classes.root} noValidate autoComplete="off">
+      <div>
+        <TextField
+          helperText="Login"
+          label="Login"
+          variant="outlined"
+        />
+      </div>
+      <div>
+        <TextField
+          helperText="Password"
+          label="Password"
+          type="password"
+          variant="outlined"
+        />
+      </div>
+      <Grid container justify = "center">
+        <Button variant="contained" className={classes.button} color="secondary">
+            Login
+          </Button>
+          <Button variant="contained" className={classes.button} color="primary">
+            Register
+          </Button>
+      </Grid>
+    </form>
+  );
+};
+
+export default LoginForm
